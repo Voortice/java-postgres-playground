b6762fbd539c9287db593b81a3b16248c2672b16
Voortice 0 <pedrohenrique2939@gmail.com>
2023-08-01 19:39:24 +0000

    p

---
+++ src/main/java/com/example/AppScanner.java
@@ -0,0 +1,28 @@
+package com.example;
+import java.util.Scanner;
+
+public class AppScanner {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        cliente.setNome(scanner.nextLine());
+
+        System.out.println("Digite a sua renda: ");
+        cliente.setRenda(scanner.nextDouble());
+
+        System.out.println("Digite o ano do seu nascimento: ");
+        cliente.setAnoNascimento(scanner.nextInt());
+
+        scanner.nextLine();
+        System.out.println("Digite o seu sexo: ");
+        String sexo = scanner.nextLine();
+        cliente.setSexo(sexo.charAt(0));
+
+        System.out.println("O nome do cliente é " + cliente.getNome());
+        System.out.println("Renda " + cliente.getRenda());
+        System.out.println("Sexo " + cliente.getSexo());
+        System.out.println("Ano de nascimento " + cliente.getAnoNascimento());
+    }
+}
