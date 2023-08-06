fac5fd0725dd12ad52ae69518f10b55120a83d1a
Voortice 0 <pedrohenrique2939@gmail.com>
2023-08-06 19:44:02 +0000

    p

--- src/main/java/com/example/AppBD.java
+++ src/main/java/com/example/AppBD.java
@@ -9,7 +9,6 @@ public class AppBD {
     private static final String SENHA = "";
     private static final String USUARIO = "gitpod";
     private static final String POSTGRESQL_URL = "jdbc:postgresql://localhost/postgres";
-    private Connection conn;
 
     public static void main(String[] args) {
         new AppBD();
@@ -18,33 +17,40 @@ public class AppBD {
     public AppBD(){
         try (var conn = getConnection()){
             carregarDriveJDBC();
-            listarEstados();  
-            localizarEstado("TO");
+            listarEstados(conn);  
+            localizarEstado(conn, "TO");
         }
         catch (SQLException e) {
             System.err.println("Não foi possivel carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
         }
     }
 
-    private void localizarEstado(String uf) {
-
+    private void localizarEstado(Connection conn, String uf) {
+        try{
+            var sql = "select * from estado where uf = ?";
+            var statement = conn.prepareStatement(sql);
+            System.out.println(sql);
+            statement.setString(1, uf);
+            var result = statement.executeQuery();
+            if(result.next()){
+                System.out.printf("id: %d Nome: %s UF: %s \n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+            }
+        } catch(SQLException e){
+            System.err.println("Erro ao executar a consulta SQL" + e.getMessage());
+        }
+        
     }
 
-    private void listarEstados() {
-        Statement statement = null;
+    private void listarEstados(Connection conn2) {
         try (var conn = getConnection()) {
             System.out.println("Conexão com o banco de dados realizada com sucesso");
-
-            statement = conn.createStatement();
+            var statement = conn.createStatement();
             var result = statement.executeQuery("select * from estado");
             while(result.next()){
                 System.out.printf("id: %d Nome: %s UF: %s \n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
             }    
         }
          catch (SQLException e) {
-            if (statement == null)
-            System.err.println("Não foi possivel carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
-            else
             System.err.println("Não foi possivel realizar a consulta ao banco de dados" + e.getMessage());
         }
     }
