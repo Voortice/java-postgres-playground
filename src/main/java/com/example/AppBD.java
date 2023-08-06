9dc9f8df43f908d8a7c40f2a1df19a8c25e0d9ca
Voortice 0 <pedrohenrique2939@gmail.com>
2023-08-06 15:03:34 +0000

    h

--- src/main/java/com/example/AppBD.java
+++ src/main/java/com/example/AppBD.java
@@ -6,16 +6,33 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class AppBD {
-    public static void main(String[] args) {
-        try {
-            Class.forName("org.postgresql.Driver");
-        }
-        catch (ClassNotFoundException e) {
-            System.err.println("Não foi possivel conectar a biblioteca para acesso ao banco de dados" + e.getMessage());
-        }
+    private static final String SENHA = "";
+    private static final String USUARIO = "gitpod";
+    private static final String POSTGRESQL_URL = "jdbc:postgresql://localhost/postgres";
+    private Connection conn;
 
+    public static void main(String[] args) {
+        new AppBD();
+    }
+
+    public AppBD(){
+        try (var conn = getConnection()){
+            carregarDriveJDBC();
+            listarEstados();  
+            localizarEstado("TO");
+        }
+        catch (SQLException e) {
+            System.err.println("Não foi possivel carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
+        }
+    }
+
+    private void localizarEstado(String uf) {
+
+    }
+
+    private void listarEstados() {
         Statement statement = null;
-        try (var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "");) {
+        try (var conn = getConnection()) {
             System.out.println("Conexão com o banco de dados realizada com sucesso");
 
             statement = conn.createStatement();
@@ -29,6 +46,19 @@ public class AppBD {
             System.err.println("Não foi possivel carregar a biblioteca para acesso ao banco de dados" + e.getMessage());
             else
             System.err.println("Não foi possivel realizar a consulta ao banco de dados" + e.getMessage());
-        }  
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(POSTGRESQL_URL, USUARIO, SENHA);
+    } 
+
+    private void carregarDriveJDBC() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            System.err.println("Não foi possivel conectar a biblioteca para acesso ao banco de dados" + e.getMessage());
+        }
     }
 }
